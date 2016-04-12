33be5870933a4b141e741b01fa33847d2845dc91
Branislav Katreniak <branislav.katreniak@digitalstrom.com>
2016-04-12 13:57:33 +0200

    cmake/macros: use in-build capnp tools if capnroto is built in cmake
    If CapnProto is part of cmake build, macro CAPNP_GENERATE_CPP
    uses in-build capnp tools.
    
    This change fixes `make check` target with cmake.
    
    All tests pass except
    1: [ FAIL ] exception-test.c++:30: legacy test:
        Exception/TrimSourceFilename
    1: /home/brano/src/capnproto/c++/src/kj/exception-test.c++:31:
        failed: expected (trimSourceFilename(
        "/home/brano/src/capnproto/c++/src/kj/exception-test.c++"))
        == ("kj/exception-test.c++");
    
    Not sure how to fix the test without disabling it.

--- c++/cmake/CapnProtoMacros.cmake
+++ c++/cmake/CapnProtoMacros.cmake
@@ -26,6 +26,15 @@ function(CAPNP_GENERATE_CPP SOURCES HEADERS)
   if(NOT ARGN)
     message(SEND_ERROR "CAPNP_GENERATE_CPP() called without any source files.")
   endif()
+  set(tool_depends ${EMPTY_STRING})
+  if(CapnProto_SOURCE_DIR)
+    #Use cmake targets if CapnProto project is part of cmake build
+    set(CAPNP_EXECUTABLE capnp_tool)
+    #We need LOCATION property of capnpc-c++, but cmake deprecated LOCATION property on targets
+    set(CAPNPC_CXX_EXECUTABLE ${CapnProto_BINARY_DIR}/src/capnp/capnpc-c++)
+    set(CAPNP_INCLUDE_DIRECTORY ${CapnProto_SOURCE_DIR}/src)
+    list(APPEND tool_depends capnp_tool capnpc_cpp)
+  endif()
   if(NOT CAPNP_EXECUTABLE)
     message(SEND_ERROR "Could not locate capnp executable (CAPNP_EXECUTABLE).")
   endif()
@@ -92,7 +101,7 @@ function(CAPNP_GENERATE_CPP SOURCES HEADERS)
           ${include_path}
           ${CAPNPC_FLAGS}
           ${file_path}
-      DEPENDS "${schema_file}"
+      DEPENDS "${schema_file}" ${tool_depends}
       COMMENT "Compiling Cap'n Proto schema ${schema_file}"
       VERBATIM
     )
